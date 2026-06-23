c3125b1cfb548a03032f74345fdc2aa8a404e2e6
techpiyush2 <your-email-used-in-github>
2026-06-24 00:57:27 +0530

    Configure Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  base: "/Tech-Piyush/",
+  base: process.env.VERCEL ? "/" : "/Tech-Piyush/",
   plugins: [
     vue(),
     glsl({
